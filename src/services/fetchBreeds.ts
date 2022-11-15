96d47d25b07e387e7a678935e3ae1631e6ae1140
muskaan bhutani <muskaan.bhutani@kongsbergdigital.com>
2022-11-15 10:19:44 +0100

    added headers

--- src/services/fetchBreeds.ts
+++ src/services/fetchBreeds.ts
@@ -1,5 +1,10 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+const requestOptions = {
+    headers: {
+    'Content-Type': 'application/json'
+    }
+}
 export const fetchBreeds = () => {
     const URL = `${BASE_URL}v1/breeds`;
-    return fetch(URL);
+    return fetch(URL, requestOptions);
 };
